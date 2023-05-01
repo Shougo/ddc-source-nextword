107049543ff9e87f697927193867419fb96704b3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-05-01 20:03:49 +0900

    Use Deno.Command()

--- denops/@ddc-sources/nextword.ts
+++ denops/@ddc-sources/nextword.ts
@@ -2,25 +2,27 @@ import {
   BaseSource,
   Context,
   Item,
-} from "https://deno.land/x/ddc_vim@v3.2.0/types.ts";
-import { readLines } from "https://deno.land/std@0.165.0/io/mod.ts";
-import { writeAll } from "https://deno.land/std@0.165.0/streams/mod.ts";
+} from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+import { TextLineStream } from "https://deno.land/std@0.185.0/streams/mod.ts";
 
 type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params> {
-  _proc: Deno.Process | undefined = undefined;
+  _proc: Deno.ChildProcess | undefined = undefined;
 
   constructor() {
     super();
 
     try {
-      this._proc = Deno.run({
-        cmd: ["nextword", "-n", "100", "-g"],
-        stdout: "piped",
-        stderr: "piped",
-        stdin: "piped",
-      });
+      this._proc = new Deno.Command(
+        "nextword",
+        {
+          args: ["-n", "100", "-g"],
+          stdout: "piped",
+          stderr: "piped",
+          stdin: "piped",
+        },
+      ).spawn();
     } catch (_e) {
       console.error('[ddc-nextword] Run "nextword" is failed');
       console.error('[ddc-nextword] "nextword" binary seems not installed');
@@ -29,18 +31,26 @@ export class Source extends BaseSource<Params> {
 
   override async gather(args: {
     context: Context;
+    completeStr: string;
   }): Promise<Item[]> {
     if (!this._proc || !this._proc.stdin || !this._proc.stdout) {
       return [];
     }
-    await writeAll(
-      this._proc.stdin,
-      new TextEncoder().encode(args.context.input + "\n"),
-    );
 
-    // Todo: Better implementation
-    for await (const line of readLines(this._proc.stdout)) {
-      return line.split(/\s/).map((word: string) => ({ word }));
+    const completeStr = args.completeStr;
+    const [sentence, offset] = extractWords(completeStr);
+    const query = offset > 0 ? sentence : args.context.input;
+    const precedingLetters = completeStr.slice(0, offset);
+
+    const writer = this._proc.stdin.getWriter();
+    await writer.ready;
+    await writer.write(new TextEncoder().encode(query + "\n"));
+    writer.releaseLock();
+
+    for await (const line of iterLine(this._proc.stdout)) {
+      return line.split(/\s/).map((word: string) => ({
+        word: precedingLetters.concat(word),
+      }));
     }
 
     return [];
@@ -49,4 +59,35 @@ export class Source extends BaseSource<Params> {
   override params(): Params {
     return {};
   }
+}
+
+async function* iterLine(r: ReadableStream<Uint8Array>): AsyncIterable<string> {
+  const lines = r
+    .pipeThrough(new TextDecoderStream(), { preventCancel: true })
+    .pipeThrough(new TextLineStream());
+
+  for await (const line of lines) {
+    if ((line as string).length) {
+      yield line as string;
+    }
+  }
+}
+
+function extractWords(
+  completeStr: string,
+): [string, number] {
+  const upperCaseRegexp = /[A-Z][A-Z]+/g;
+  const camelCaseRegexp = /([A-Z]?[a-z]+|[A-Z][a-z]*)/g; // Also matched to PascalCase
+  const snakeCaseRegexp = /[a-z][a-z]*/g; // Also matched to kebab-case, etc.
+  let matches: string[] | null = completeStr.match(upperCaseRegexp);
+  if (matches === null) matches = completeStr.match(camelCaseRegexp);
+  if (matches === null) matches = completeStr.match(snakeCaseRegexp);
+  if (matches === null) return [completeStr, 0];
+  const sentence = matches.join(" ");
+  if (completeStr.match(/[^a-zA-Z]+$/)) {
+    return [sentence.concat(" "), completeStr.length];
+  }
+  const lastWord = matches.at(-1) || completeStr;
+  const offset = completeStr.lastIndexOf(lastWord);
+  return [sentence, offset];
 }
